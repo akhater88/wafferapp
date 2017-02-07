b3a1820d5cb53cdfdbc4d05d28b6238a0cb89cb3
abd <abd@abd>
2017-02-07 22:27:41 +0200

    WafferApp

--- views/error404.php
+++
@@ -1,3 +0,0 @@
-<h1><?php echo $blog_heading; ?></h1>
-<p>You have reached invalid page</p>
-<p>Please check your URL</p>
--- views/pages/display_page_empty.php
+++
@@ -1,5 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<div id="content-inner">
-<span class="title-inner">No page was found....</span>
-<br />
-</div>
