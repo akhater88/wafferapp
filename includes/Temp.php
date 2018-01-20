4bfa035bc901e262a186e989b7354f6e361f2070
Abdullah Mohammed <abdullah.mohammed@maf.co.ae>
2018-01-20 13:05:29 +0200

    New Theme

--- includes/Temp.php
+++ includes/Temp.php
@@ -43,7 +43,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="" /></a>
+                        <a class="navbar-brand" href="index.html"><img src="<?php echo __SCRIPT_PATH;?>assets/images/logo.png" alt="" /></a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -58,7 +58,7 @@
                             <li><a href="#">Contact Us</a></li>
 
                             <div class="contact-number text-right"> 
-                                <ul><li class="">(+001122334)</li></ul>
+                                <ul><li class="">info@wafferapp.com</li></ul>
                             </div>
 
                         </ul>
