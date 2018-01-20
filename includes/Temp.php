4f3b08ed407fef3a6d62f7e77435342d61a61ca3
Abdullah Mohammed <abdullah.mohammed@maf.co.ae>
2018-01-20 13:27:50 +0200

    New Theme

--- includes/Temp.php
+++ includes/Temp.php
@@ -43,7 +43,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#"><img src="<?php echo __SCRIPT_PATH;?>assets/images/logo.png" alt="" style="height: 100px"/>WafferApp</a>
+                        <a class="navbar-brand" href="#"><img src="<?php echo __SCRIPT_PATH;?>assets/images/logo_waffer.png" alt="" style="height: 100px"/>WafferApp</a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
